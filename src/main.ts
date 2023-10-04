c52e1cfda1fbbeb2943bfabcbd3c18e9251c6cc0
Steve Crow <steve@smcrow.com>
2023-10-03 21:12:36 -0400

    Add frequency and filter out observers from controller list.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import * as dotenv from "dotenv";
+import { Controller, fetchControllers } from "./vatsim";
 
 dotenv.config();
-
-import { Controller, fetchControllers } from "./vatsim";
-import { pick } from "lodash";
 
 let controllers: Controller[] = [];
 
@@ -14,23 +12,22 @@ async function main() {
 
 async function reportControllerChanges() {
   const now = new Date();
-  const newControllers = await fetchControllers();
+  const newControllers = (await fetchControllers()).filter((c) => c.rating.short !== "OBS");
   // Generate a list of controllers that have been added or removed
+
   const differences = {
     added: newControllers.filter(c => !controllers.find((c2) => c2.cid == c.cid)),
-    removed: controllers.filter((c) => !newControllers.find((c2) => c2.cid == c.cid)),
-  }
+    removed: controllers.filter((c) => !newControllers.find((c2) => c2.cid == c.cid))
+  };
 
   // Report on differences
   differences.added.forEach((c) => {
-    console.log(`${now.toISOString()} [SIGNED ON] [${c.callsign}] ${c.cid} ${c.name} (${c.rating.short})`);
-  })
+    console.log(`${now.toISOString()} [SIGNED ON] [${c.callsign} ${c.facility.short}] ${c.cid} ${c.name} (${c.rating.short}) ${c.frequency}`);
+  });
 
   differences.removed.forEach((c) => {
-    console.log(`${now.toISOString()} [SIGNED OFF] [${c.callsign}] ${c.cid} ${c.name} (${c.rating.short})`);
-  })
-
-  console.log(`${now.toISOString()} [ ${differences.added.length} Signed On | ${differences.removed.length} Signed Off ]`)
+    console.log(`${now.toISOString()} [SIGNED OFF] [${c.callsign} ${c.facility.short}] ${c.cid} ${c.name} (${c.rating.short})`);
+  });
 
   controllers = newControllers;
 }
